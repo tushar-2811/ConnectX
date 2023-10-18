4ba17d4c7cf1501ba45107ce722db6e50870470b
Tushar Rawat <tusharrawat52@gmail.com>
2023-10-18 20:45:51 +0530

    initialized project

--- Server/routes/api/v1/v1IndexRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRouter from "./userRoutes/userRoutes";
-
-const v1Router = Router();
-
-v1Router.use('/user' , userRouter);
-
-export default v1Router;
--- Server/routes/indexRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import apiRouter from "./api/apiIndexRoutes";
-
-const indexRouter = Router();
-
-indexRouter.use('/api' , apiRouter);
-
-
-export default indexRouter;
